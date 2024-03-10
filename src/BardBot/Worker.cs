7d2545746519c2d0ccca4f7b6b225cc59d70d16c
matrumz <matrumz@hotmail.com>
2024-03-10 03:54:20 +0000

    feat: demo bot login

--- src/BardBot/Worker.cs
+++ src/BardBot/Worker.cs
@@ -1,20 +1,33 @@
+using Discord;
+using Discord.WebSocket;
+
 namespace BardBot;
 
 public class Worker : BackgroundService
 {
+    private readonly IConfiguration _config;
     private readonly ILogger<Worker> _logger;
+    private readonly DiscordSocketClient _client;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(IConfiguration config, ILogger<Worker> logger)
     {
+        _config = config;
         _logger = logger;
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        _logger.LogInformation(msg.Message);
+        return Task.CompletedTask;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
-        }
+        await _client.LoginAsync(TokenType.Bot, _config["Bot:Token"]);
+        await _client.StartAsync();
+
+        await Task.Delay(-1, stoppingToken);
     }
 }
