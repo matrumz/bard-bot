1c4024c598ef806a2965fb4876e38f31c8c11981
matrumz <matrumz@hotmail.com>
2024-03-10 03:05:12 +0000

    feat: add starter BardBot project
    ```
    dotnet new worker --use-program-main
    dotnet sln add src/BardBot/BardBot.csproj
    ```

---
+++ src/BardBot/Program.cs
@@ -0,0 +1,16 @@
+namespace BardBot;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+            })
+            .Build();
+
+        host.Run();
+    }
+}
