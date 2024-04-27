3e94e64cc01e8fa2fb6b740fffcb3a5469e1cc08
matrumz <matrumz@hotmail.com>
2024-04-27 05:35:07 +0000

    wip: somehow forgot to add appsettings.local.json to program?

--- src/BardBot/Program.cs
+++ src/BardBot/Program.cs
@@ -6,6 +6,7 @@ public class Program
 {
     public static void Main(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) => config.AddJsonFile("appsettings.local.json", optional: true))
             .UseStartup<Discord.Hosting.Startup>()
             .Build()
             .Run();
