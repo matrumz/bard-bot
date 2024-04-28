2cf86ea8200655a1fe57c12af65ed60de255d6ef
matrumz <matrumz@hotmail.com>
2024-04-28 23:36:30 +0000

    wip

--- src/BardBot.Discord/Hosting/Startup.cs
+++ src/BardBot.Discord/Hosting/Startup.cs
@@ -14,18 +14,17 @@ public sealed class Startup : IStartup
 {
     public void ConfigureServices(HostBuilderContext hostBuilderContext, IServiceCollection services)
     {
-        var discordSocketConfig = new DiscordSocketConfig()
-        {
-            UseInteractionSnowflakeDate = false, // My computer system clock often drifts and I cannot vouch for the accuracy of my users' system clocks. This will prevent the bot from rejecting interactions that are "too old" when they are not.
-        };
-
         services.AddHostedService<ConnectionHandler>();
         services.AddHostedService<InteractionHandler>();
         services.AddOptions<DiscordOptions>()
             .BindConfiguration(DiscordOptions.Discord)
             .ValidateDataAnnotations()
             .AddValueAsSingleton();
-        services.AddSingleton(discordSocketConfig);
+        services.AddSingleton(new DiscordSocketConfig()
+        {
+            UseInteractionSnowflakeDate = false // My computer system clock often drifts and I cannot vouch for the accuracy of my users' system clocks. This will prevent the bot from rejecting interactions that are "too old" when they are not.
+        });
+        services.AddSingleton(new InteractionServiceConfig() { DefaultRunMode = RunMode.Async });
         services.AddSingleton<DiscordSocketClient>();
         services.AddSingleton<InteractionService>();
     }
