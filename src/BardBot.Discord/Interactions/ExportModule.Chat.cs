fcb53631fc2b2105c013672ed90acce29c1c31a0
matrumz <matrumz@hotmail.com>
2024-04-28 23:30:07 +0000

    wip: modal sort of working

--- src/BardBot.Discord/Interactions/ExportModule.Chat.cs
+++ src/BardBot.Discord/Interactions/ExportModule.Chat.cs
@@ -1,12 +1,41 @@
+using Discord;
 using Discord.Interactions;
-using Discord.Interactions.Builders;
 
 namespace BardBot.Discord.Interactions;
 
-public partial class ExportModule
+public sealed partial class ExportModule
 {
     [SlashCommand("chat", "Configured channels & threads.")]
-    public async Task ExportChatAsync()
+    public async Task ExportChatAsync() =>
+        await Context.Interaction.RespondWithModalAsync<ChatExportModal>(ChatExportModal.CustomId);
+
+    public sealed class ChatExportModal : IModal
     {
+        public const string CustomId = "chat_export_modal";
+
+        public string Title => "Export Chat";
+
+        [InputLabel("After Date")]
+        [RequiredInput(false)]
+        [ModalTextInput("after_date", style: TextInputStyle.Short, placeholder: @"""last"" or a date with time")]
+        public string AfterDate { get; set; }
+
+        [InputLabel("Before Date")]
+        [RequiredInput(false)]
+        [ModalTextInput("before_date", style: TextInputStyle.Short, placeholder: @"""now"" or a date with time")]
+        public string BeforeDate { get; set; }
+
+        [InputLabel("Bulk Export")]
+        [RequiredInput(false)]
+        [ModalTextInput("bulk_export", style: TextInputStyle.Paragraph, placeholder: "CSV of after/before dates")]
+        public string BulkExport { get; set; }
+
     }
+
+    [ModalInteraction(ChatExportModal.CustomId)]
+    public async Task ModalResponseAsync(ChatExportModal modal)
+    {
+        await RespondAsync(modal.BulkExport);
+    }
+
 }
