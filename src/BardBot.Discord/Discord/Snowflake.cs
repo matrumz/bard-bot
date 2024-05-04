0645e2aad51234420d2536bd8b48b1b1d7af0ece
matrumz <matrumz@hotmail.com>
2024-05-04 02:55:19 +0000

    wip(discord): add implicit casting for snowflake

--- src/BardBot.Discord/Discord/Snowflake.cs
+++ src/BardBot.Discord/Discord/Snowflake.cs
@@ -1,3 +1,9 @@
 namespace BardBot.Discord.Discord;
 
-internal partial record Snowflake(ulong Value);
+internal partial record Snowflake(
+    ulong Value
+)
+{
+    public static implicit operator ulong(Snowflake snowflake) => snowflake.Value;
+    public static implicit operator Snowflake(ulong value) => new(value);
+}
