f4b11525fe5354001700559aac1e9fe5595a6599
matrumz <matrumz@hotmail.com>
2024-05-24 02:55:51 +0000

    wip(discord): enhance snowflake

--- src/BardBot.Discord/Discord/Snowflake.cs
+++ src/BardBot.Discord/Discord/Snowflake.cs
@@ -1,9 +1,32 @@
+using Discord;
+
 namespace BardBot.Discord.Discord;
 
-internal partial record Snowflake(
-    ulong Value
-)
+internal partial record Snowflake(ulong Value);
+
+// As ulong
+internal partial record Snowflake
 {
     public static implicit operator ulong(Snowflake snowflake) => snowflake.Value;
     public static implicit operator Snowflake(ulong value) => new(value);
+}
+
+// As DateTimeOffset
+internal partial record Snowflake
+{
+    public static implicit operator DateTimeOffset(Snowflake snowflake) => SnowflakeUtils.FromSnowflake(snowflake.Value);
+    public static implicit operator Snowflake(DateTimeOffset value) => new(SnowflakeUtils.ToSnowflake(value));
+}
+
+
+// Comparison
+internal partial record Snowflake : IComparable<Snowflake>, IComparable
+{
+    public int CompareTo(Snowflake? other) => Value.CompareTo(other?.Value);
+    public int CompareTo(object? obj) =>
+        obj is Snowflake other
+            ? Value.CompareTo(other.Value)
+            : throw new ArgumentException($"Object must be of type {nameof(Snowflake)}");
+    public static bool operator >(Snowflake left, Snowflake right) => left.CompareTo(right) > 0;
+    public static bool operator <(Snowflake left, Snowflake right) => left.CompareTo(right) < 0;
 }
