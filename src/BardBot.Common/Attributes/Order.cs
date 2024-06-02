c4ce14411a538c4ae86d1ee4c89fd20f0a9c778b
matrumz <matrumz@hotmail.com>
2024-06-02 19:43:34 +0000

    wip(common): add order attribute

---
+++ src/BardBot.Common/Attributes/Order.cs
@@ -0,0 +1,26 @@
+using System.Runtime.CompilerServices;
+
+namespace BardBot.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public sealed class OrderAttribute(
+    [CallerFilePath] string file = "",
+    [CallerLineNumber] int line = 0
+) : Attribute
+{
+    public string File { get => file; }
+    public int Order { get => line; }
+}
+
+public interface IContainsOrderedProperties
+{
+    public IReadOnlyList<(string, object?)> OrderedProperties() =>
+        GetType().GetProperties()
+            .Where(property => Attribute.IsDefined(property, typeof(OrderAttribute)))
+            .Select(property => (property, attribute: (OrderAttribute)Attribute.GetCustomAttribute(property, typeof(OrderAttribute))!))
+            .OrderBy(pa => pa.attribute.File)
+            .ThenBy(pa => pa.attribute.Order)
+            .Select(pa => (pa.property.Name, pa.property.GetValue(this)))
+            .ToArray()
+        ;
+}
