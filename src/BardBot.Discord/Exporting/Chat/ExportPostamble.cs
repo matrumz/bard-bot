43ea746fbe15e61bc011b62b99a12c8cbdc75fce
matrumz <matrumz@hotmail.com>
2024-06-02 20:17:48 +0000

    wip(discord): more plaintext exporting

--- src/BardBot.Discord/Exporting/Chat/ExportPostamble.cs
+++ src/BardBot.Discord/Exporting/Chat/ExportPostamble.cs
@@ -1,3 +1,16 @@
+using System.Reflection;
+
+using BardBot.Common.Attributes;
+
 namespace BardBot.Discord.Exporting.Chat;
 
-internal record ExportPostamble();
+internal partial record ExportPostamble();
+
+internal partial record ExportPostamble : IContainsOrderedProperties
+{
+
+    public IReadOnlyList<PropertyInfo> OrderedProperties() =>
+        // Create an instance of the interface and call the default method
+        ((IContainsOrderedProperties)this).OrderedProperties();
+
+}
--- src/BardBot.Discord/Exporting/Chat/MessageFormatter.PlainText.cs
+++ src/BardBot.Discord/Exporting/Chat/MessageFormatter.PlainText.cs
@@ -1,6 +1,9 @@
 
 using System.Text;
 
+using BardBot.Common.Attributes;
+using BardBot.Common.Extensions;
+
 using Discord;
 
 namespace BardBot.Discord.Exporting.Chat;
@@ -14,20 +17,36 @@ internal class PlainTextMessageFormatter : IMessageFormatter
         sb.AppendLine(MessageHeader(message));
         sb.AppendLine("---");
         sb.AppendLine(message.Content);
+        Attachments(message).IfNotNull(attachments => sb.Append(attachments));
+        return new(sb.ToString());
     }
 
-    ValueTask<string> IMessageFormatter.FormatPreambleAsync(ExportPreamble preamble, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+    ValueTask<string> IMessageFormatter.FormatPreambleAsync(ExportPreamble preamble, CancellationToken cancellationToken = default) =>
+        new(Amble(preamble));
 
-    ValueTask<string> IMessageFormatter.FormatPostambleAsync(ExportPostamble postamble, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+
+    ValueTask<string> IMessageFormatter.FormatPostambleAsync(ExportPostamble postamble, CancellationToken cancellationToken = default) =>
+        new(Amble(postamble));
 
     ValueTask<string> IMessageFormatter.SpacerAsync(CancellationToken cancellationToken) =>
         new("\n\n");
+
+    private static string AmbleBoundary { get; } = "=".Repeat(80);
+
+    private static string Amble(IContainsOrderedProperties amble)
+    {
+        var props = amble.OrderedProperties();
+        return props.Count > 0
+            ? new StringBuilder()
+                .AppendLine(AmbleBoundary)
+                .AppendJoin('\n', props
+                    .Select(property => $"{property.Name}: {property.GetValue(amble)?.ToString() ?? ""}")
+                )
+                .Append(AmbleBoundary)
+                .ToString()
+            : string.Empty
+            ;
+    }
 
     private static string DateTime__String(DateTimeOffset dto) =>
         DateTime__String(dto.DateTime);
@@ -38,6 +57,15 @@ internal class PlainTextMessageFormatter : IMessageFormatter
     private static string MessageHeader(IMessage message) =>
         $"[{DateTime__String(message.Timestamp)}] {message.Author.Username}";
 
-    private static string
+    private static string? Attachments(IMessage message) =>
+        message.Attachments.Count > 0
+        ? new StringBuilder()
+            .AppendLine()
+            .AppendJoin('\n', message.Attachments
+                .Select(attachment => $"[Attachment: {attachment.Title} -- {attachment.Url} -- {attachment.Description}]")
+            )
+            .ToString()
+        : null
+        ;
 
 }
